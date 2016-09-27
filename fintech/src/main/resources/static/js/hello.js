9a8e2d0e4dfe96b6c4676d3462cec4043758b29b
yuri Brandão <yurisbv@gmail.com>
2016-09-27 19:57:39 -0300

    Primeiro Commit
    O que eu fiz:
    fonte:  https://spring.io/guides/tutorials/spring-security-and-angular-js/
    https://spring.io/blog/2015/01/12/spring-and-angular-js-a-secure-single-page-application
    Resolvendo as dependências para o gradle:
    Usar o  wro4j para trabalhar com modificações no CSS de alto nível como a linguagem LESS. Também é um minificador de código javascript.
    >> Solução para o gradle https://github.com/IlyaAI/wro4j-gradle-plugin
    Ao executar o projeto, acessando o localhost será pedido um usuário e senha. O usuário é ‘user’ e a senha está impressa no terminal.
    
    Parte 2 iniciada
    https://spring.io/blog/2015/01/12/the-login-page-angular-js-and-spring-security-part-ii

---
+++ fintech/src/main/resources/static/js/hello.js
@@ -0,0 +1,21 @@
+angular.module('hello', [ 'ngRoute' ])
+.config(function($routeProvider, $httpProvider) {
+
+	$routeProvider.when('/', {
+		templateUrl : '../templates/home.html',
+		controller : 'home'
+	}).when('/login', {
+		templateUrl : 'login.html',
+		controller : 'navigation'
+	}).otherwise('/');
+
+  $httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
+
+})
+
+.controller('home', function($scope, $http) {
+  $http.get('/resource/').success(function(data) {
+    $scope.greeting = data;
+  })
+})
+.controller('navigation', function() {});
